4a8e3cb3856ead9f8a2e376964a5a6d9a305337e
rostovcevartem209 <rostovcevartem209@gmail.com>
2026-09-12 19:33:21 +0300

    реализован полный цикл генетического алгоритма

--- main.cpp
+++ main.cpp
@@ -14,32 +14,59 @@ int main() {
     dm.loadTestData();
 
     std::cout << "Система автоматической генерации расписания\n";
-    std::cout << "Разработчики студенты 2 курса Ростовцев Артём, Кулик Макар, Фишман Илья\n";
+    std::cout << "Разработчики Роман Хориков студент 1 курса Ростовцев Артём Кулик Макар Фишман Илья\n";
     std::cout << "--------------------------------------------------\n";
 
-    // создаем переменные для хранения лучшего результата
-    std::vector<Gene> bestSchedule;
-    int minPenalty = 999999;
+    // создаем начальную популяцию из пятидесяти расписаний
+    int populationSize = 50;
+    std::vector<std::vector<Gene>> population(populationSize);
 
-    // генерируем 1000 случайных вариантов
-    for (int i = 1; i <= 1000; ++i) {
-
-        // создаем случайное расписание
-        std::vector<Gene> currentSchedule = GeneticAlgorithm::generateRandomSchedule(dm);
-
-        // считаем штрафные баллы
-        int currentPenalty = Fitness::calculatePenalty(currentSchedule);
-
-        // если этот вариант лучше предыдущих сохраняем его
-        if (currentPenalty < minPenalty) {
-            minPenalty = currentPenalty;
-            bestSchedule = currentSchedule;
-        }
+    for (int i = 0; i < populationSize; ++i) {
+        population[i] = GeneticAlgorithm::generateRandomSchedule(dm);
     }
 
-    // выводим итог на экран
-    std::cout << "Проверено 1000 вариантов расписания\n";
-    std::cout << "Лучший результат имеет штраф " << minPenalty << " баллов\n";
+    // переменные для хранения лучшего результата за все поколения
+    std::vector<Gene> bestSchedule = population[0];
+    int bestPenalty = Fitness::calculatePenalty(bestSchedule);
+
+    // запускаем цикл эволюции на сто поколений
+    int generations = 100;
+    for (int gen = 0; gen < generations; ++gen) {
+
+        // проверяем каждое расписание в популяции
+        for (int i = 0; i < populationSize; ++i) {
+            int penalty = Fitness::calculatePenalty(population[i]);
+
+            // если нашли вариант лучше сохраняем его
+            if (penalty < bestPenalty) {
+                bestPenalty = penalty;
+                bestSchedule = population[i];
+            }
+        }
+
+        // если нашли идеальное расписание без штрафов прерываем цикл
+        if (bestPenalty == 0) {
+            break;
+        }
+
+        // создаем новое поколение методом скрещивания и мутации
+        std::vector<std::vector<Gene>> newPopulation(populationSize);
+        for (int i = 0; i < populationSize; ++i) {
+            std::vector<Gene> parent1 = population[0];
+            std::vector<Gene> parent2 = population[1];
+
+            std::vector<Gene> child = GeneticAlgorithm::crossover(parent1, parent2);
+            GeneticAlgorithm::mutate(child, dm);
+
+            newPopulation[i] = child;
+        }
+
+        population = newPopulation;
+    }
+
+    // выводим итоговые результаты работы генетического алгоритма
+    std::cout << "Эволюция завершена успешно\n";
+    std::cout << "Лучший штрафной балл " << bestPenalty << "\n";
     std::cout << "Количество жестких конфликтов " << Validator::countHardConflicts(bestSchedule) << "\n";
 
     return 0;
