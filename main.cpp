ce2ec00d9cd3e8ec5387a85cabd050de7892772b
rostovcevartem209 <rostovcevartem209@gmail.com>
2026-09-12 19:24:01 +0300

    Добавлен поиск лучшего расписания из 1000 вариантов

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "data_manager.h"
 #include "validator.h"
 #include "genetic.h"
+#include "fitness.h"
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -13,22 +14,33 @@ int main() {
     dm.loadTestData();
 
     std::cout << "Система автоматической генерации расписания\n";
-    std::cout << "Разработчики Ростовцев Артём, Кулик Макар, Фишман Илья\n";
+    std::cout << "Разработчики студенты 2 курса Ростовцев Артём, Кулик Макар, Фишман Илья\n";
     std::cout << "--------------------------------------------------\n";
 
-    // генерируем 10 случайных расписаний
-    for (int i = 1; i <= 10; ++i) {
+    // создаем переменные для хранения лучшего результата
+    std::vector<Gene> bestSchedule;
+    int minPenalty = 999999;
 
-        // создаем случайный вариант
-        std::vector<Gene> randomSchedule = GeneticAlgorithm::generateRandomSchedule(dm);
+    // генерируем 1000 случайных вариантов
+    for (int i = 1; i <= 1000; ++i) {
 
-        // считаем ошибки
-        int conflicts = Validator::countHardConflicts(randomSchedule);
+        // создаем случайное расписание
+        std::vector<Gene> currentSchedule = GeneticAlgorithm::generateRandomSchedule(dm);
 
-        // выводим результат на экран
-        std::cout << "Вариант " << i << " создано занятий " << randomSchedule.size()
-            << " конфликтов найдено " << conflicts << "\n";
+        // считаем штрафные баллы
+        int currentPenalty = Fitness::calculatePenalty(currentSchedule);
+
+        // если этот вариант лучше предыдущих сохраняем его
+        if (currentPenalty < minPenalty) {
+            minPenalty = currentPenalty;
+            bestSchedule = currentSchedule;
+        }
     }
+
+    // выводим итог на экран
+    std::cout << "Проверено 1000 вариантов расписания\n";
+    std::cout << "Лучший результат имеет штраф " << minPenalty << " баллов\n";
+    std::cout << "Количество жестких конфликтов " << Validator::countHardConflicts(bestSchedule) << "\n";
 
     return 0;
 }
