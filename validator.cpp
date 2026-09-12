c6cdfe76f9ad9f716342554421e0bd4c90b24c18
rostovcevartem209 <rostovcevartem209@gmail.com>
2026-09-12 18:56:06 +0300

    Добавлен класс Validator для проверки жестких конфликтов.

---
+++ validator.cpp
@@ -0,0 +1,33 @@
+#include "validator.h"
+
+int Validator::countHardConflicts(const std::vector<Gene>& schedule) {
+    int conflicts = 0;
+    int totalGenes = schedule.size();
+
+    // Сравниваем каждую пару с каждой другой
+    for (int i = 0; i < totalGenes; ++i) {
+        for (int j = i + 1; j < totalGenes; ++j) {
+
+            // Если две пары проходят в одно и то же время
+            if (schedule[i].timeSlotId == schedule[j].timeSlotId) {
+
+                // Проверка 1: Одна группа на двух занятиях
+                if (schedule[i].groupId == schedule[j].groupId) {
+                    conflicts++;
+                }
+
+                // Проверка 2: Один преподаватель ведет две пары
+                if (schedule[i].teacherId == schedule[j].teacherId) {
+                    conflicts++;
+                }
+
+                // Проверка 3: Две разные группы в одной аудитории
+                if (schedule[i].roomId == schedule[j].roomId) {
+                    conflicts++;
+                }
+            }
+        }
+    }
+
+    return conflicts;
+}
--- validator.h
+++ validator.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "models.h"
+#include <vector>
+
+class Validator {
+public:
+    // Функция принимает готовое расписание и возвращает количество конфликтов
+    static int countHardConflicts(const std::vector<Gene>& schedule);
+};
